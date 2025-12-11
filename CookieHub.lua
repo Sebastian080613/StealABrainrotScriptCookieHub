e975bbfc18218c9845fbfda34a48c2c5d7510add
Sebastian080613 <sebastianmickler13@gmail.com>
2025-12-10 18:10:23 -0600

    Implement notification on script execution
    Added notification for executing the Cookie Hub Script.

--- CookieHub.lua
+++ CookieHub.lua
@@ -33,9 +33,18 @@ local Window = Rayfield:CreateWindow({
       FileName = "CookieHubKey.text", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"hi"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"hi"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22"
+
+      Rayfield:Notify({
+   Title = "You executed the Cookie Hub Script",
+   Content = "We have many different features",
+   Duration = 6.5,
+   Image = 4483362458,
+})
+      )
    }
 })
+
 
 local MainTab = Window:CreateTab("🏠 Main", 4483362458) -- Title, Image
 local MainSection = Tab:CreateSection("Main")
